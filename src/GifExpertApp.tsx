a0862be1134c18910b48681328b98f160836bf16
Guillermo Franco <fra_borjon@hotmail.com>
2025-03-24 17:02:26 -0600

    styles

---
+++ src/GifExpertApp.tsx
@@ -0,0 +1,13 @@
+import { useState } from "react";
+
+export const GifExpertApp:React.FC = () => {
+
+  const [categories, setCategories] = useState('');
+
+  return (
+    <>
+      <h1>Gif Expert App</h1>
+    </>
+  )
+}
+
